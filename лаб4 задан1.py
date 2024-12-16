745b3442309ea20724ef44d9afec5ad5c915c78b
Орлов Даниил <145991779+qpdle@users.noreply.github.com>
2024-12-16 22:41:03 +0300

    Add files via upload

---
+++ лаб4 задан1.py
@@ -0,0 +1,17 @@
+#1 задан
+from math import*
+import datetime
+a=int(input())
+date=datetime.datetime.now()
+print(sqrt(a))
+print(date)
+
+#2 задан
+import my_module
+my_module.sum(10,12)
+
+#3 задан
+import mathematika.operations
+mathematika.operations.proizv(10,7)
+import mathematika.prost
+mathematika.prost.prost(181)
